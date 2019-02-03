7c56b4fa35957743d7f928a21c7115e99a8e31c9
nathaniel <348831271@qq.com>
2019-02-04 04:13:42 +0800

    ..

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from database import *
 import datetime
 
 
-result = Bin.select('EURUSD',datetime.datetime(2018,1,1,0,0,0),datetime.datetime(2018,1,5,0,0,0))
+result = Bin.select('EURUSD',from_date=datetime.datetime(2018,1,1,0,0,0),to_date=datetime.datetime(2018,1,5,0,0,0))
 print(result)
