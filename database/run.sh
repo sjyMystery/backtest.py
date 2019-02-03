25e28f0070142eee20c53f5b0ece7b6c58d20d3f
nathaniel <348831271@qq.com>
2019-02-04 06:26:42 +0800

    123

--- database/run.sh
+++ database/run.sh
@@ -3,7 +3,7 @@ if [! -d "/var/lib/db_mysql"];then
 fi
 
 rm -rf /var/lib/db_mysql/gran.sql
-cp -f ./gran.sql /var/lib/db_mysql/
+cp -f ./gran.sql /var/lib/db_mysql/gran.sql
 
 docker run --name db_mysql -e MYSQL_ROOT_PASSWORD=123456 -v /var/lib/db_mysql:/var/lib/mysql -p 3306:3306 -d mysql
 
