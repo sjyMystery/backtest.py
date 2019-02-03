e4757195264389810b88ac2dc9f5454f79110a06
nathaniel <348831271@qq.com>
2019-02-04 03:53:53 +0800

    fix a litte bug.

--- bin.py
+++ bin.py
@@ -10,12 +10,12 @@ class Bin(Base):
 
     ask_open = Column(Float)
     ask_low = Column(Float)
-    ask_heigh = Column(Float)
+    ask_high = Column(Float)
     ask_close = Column(Float)
 
     bid_open = Column(Float)
     bid_low = Column(Float)
-    bid_heigh = Column(Float)
+    bid_high = Column(Float)
     bid_close = Column(Float)
 
     start_date = Column(DateTime,primary_key=True)
