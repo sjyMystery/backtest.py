75de72e50a7ce842c45e083f2ebb284ae48b9aeb
nathaniel <348831271@qq.com>
2019-02-04 02:35:19 +0800

    commit.

--- bin.py
+++ bin.py
@@ -1,1 +1,18 @@
-from database import db
+from database import *
+class Bin(Base):
+    __tablename__='user'
+    id = Column(Integer,primary_key=True)
+    type = Column(String(20),primary_key=True)
+
+    ask_open = Column(Float)
+    ask_low = Column(Float)
+    ask_heigh = Column(Float)
+    ask_close = Column(Float)
+
+    bid_open = Column(Float)
+    bid_low = Column(Float)
+    bid_heigh = Column(Float)
+    bid_close = Column(Float)
+
+    start_date = Column(DateTime,primary_key=True)
+    end_time = Column(DateTime,primary_key=True)
--- database.py
+++ database.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func,Float
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 from config import *
 
 engine = create_engine(f'mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+DBSession = sessionmaker(bind=engine)
