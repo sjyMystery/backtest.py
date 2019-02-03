91bf1e383926c9729b139dc90ca458b7eca1f82a
nathaniel <348831271@qq.com>
2019-02-04 07:35:15 +0800

    11

--- database/run.sh
+++ database/run.sh
@@ -17,4 +17,6 @@ cp ./gran.sql $temp_path/gran.sql
 cp ./grant.sh $temp_path/grant.sh
 docker run -it --name $docker_name -e MYSQL_ROOT_PASSWORD=$password -v $temp_path:/fuck_script_saving_path -p $expose_port:3306 -d mysql:5.6
 
-docker exec -ti -w /fuck_script_saving_path $docker_name sh -c "mysql $password -h 127.0.0.1 -uroot -p123456"
+
+# temporaily deleted this line....
+# docker exec -ti -w /fuck_script_saving_path $docker_name sh -c "mysql $password -h 127.0.0.1 -uroot -p123456"
