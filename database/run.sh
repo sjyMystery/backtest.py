054ac28cd1d8e60e5931664e5088d5d6073a06d7
nathaniel <348831271@qq.com>
2019-02-04 06:31:12 +0800

    123

--- database/run.sh
+++ database/run.sh
@@ -1,3 +1,6 @@
+$docker_name = db_mysql
+$password = 123456
+
 if [ ! -d "/var/lib/db_mysql" ]; then
     mkdir /var/lib/db_mysql
 fi
@@ -5,6 +8,6 @@ fi
 rm -rf /var/lib/db_mysql/gran.sql
 cp -f ./gran.sql /var/lib/db_mysql/gran.sql
 
-docker run --name db_mysql -e MYSQL_ROOT_PASSWORD=123456 -v /var/lib/db_mysql:/var/lib/mysql -p 3306:3306 -d mysql
+docker run --name $docker_name -e MYSQL_ROOT_PASSWORD=123456 -v /var/lib/db_mysql:/var/lib/mysql -p 3306:3306 -d mysql
 
-docker exec db_mysql "bash /var/lib/mysql/gran.sql"
+docker exec $docker_name "mysql -u root -p $password -h 127.0.0.1  /var/lib/mysql/gran.sql"
