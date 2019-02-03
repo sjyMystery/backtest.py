d166e48acc31626a617e854022e80b74259f3624
nathaniel <348831271@qq.com>
2019-02-04 03:56:53 +0800

    just a simple test.

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ bin1 = Bin(type='EURUSD',start_date=datetime.datetime(2015,1,1,0,0,0))
 
 session = DBSession()
 
-result = session.query(Bin).filter(Bin.type=='EURUSD').filter(Bin.start_date==datetime.datetime(2015,1,1,0,0,0)).one()
+result = session.query(Bin).filter(Bin.type=='AUDCAD').filter(Bin.start_date>=datetime.datetime(2015,1,1,0,0,0)).count()
+
+print(result)
