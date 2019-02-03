6a0a506aa7a3847d5dbe4a3b59ab3d1263a062f7
nathaniel <348831271@qq.com>
2019-02-04 03:28:51 +0800

    .

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ bin1 = Bin(type='EURUSD',start_date=datetime.datetime(2015,1,1,0,0,0))
 
 session = DBSession()
 
-result = session.query(bin1).all()
-print(result)
+result = session.query(Bin,type='EURUSD',start_date=datetime.datetime(2015,1,1,0,0,0)).all()
