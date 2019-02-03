10fc7a860ad9899c5d583aa07e63dd86afea5149
nathaniel <348831271@qq.com>
2019-02-04 03:35:49 +0800

    ..

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ bin1 = Bin(type='EURUSD',start_date=datetime.datetime(2015,1,1,0,0,0))
 
 session = DBSession()
 
-result = session.query(Bin).filter(type='EURUSD',start_date=datetime.datetime(2015,1,1,0,0,0)).all()
+result = session.query(Bin).filter(User.type=='EURUSD').filter(User.start_date==datetime.datetime(2015,1,1,0,0,0)).one()
